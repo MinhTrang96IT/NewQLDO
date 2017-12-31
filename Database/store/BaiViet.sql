b57d6c546bd9c010a19f15beca3fe7f3f5386da5
HieuQB <hieuit275@gmail.com>
2017-12-31 11:58:32 +0700

    cap nhat db

--- Database/store/BaiViet.sql
+++ Database/store/BaiViet.sql
@@ -6,16 +6,17 @@ GO
 
 CREATE PROC sp_BaiViet_LayDanhSachTheoTenNhanVien
 AS
-	SELECT MaBaiViet, MaChiTietQuangCao, TenNhanVien, NoiDungBaiViet, TrangThaiKiemDuyet FROM BaiViet, NhanVien WHERE BaiViet.MaNhanVien = NhanVien.MaNhanVien
+	SELECT MaBaiViet, MaChiTietQuangCao, TenNhanVien, NoiDungBaiViet, TrangThaiKiemDuyet, LyDoKhongDuyet FROM BaiViet, NhanVien WHERE BaiViet.MaNhanVien = NhanVien.MaNhanVien
 GO
 
 CREATE PROC sp_BaiViet_Them
 	@machitietquangcao int,				
 	@manhanvien int,
 	@noidungbaiviet	nvarchar(4000),
-	@trangthaikiemduyet	int		
+	@trangthaikiemduyet	int,
+	@lydokhongduyet nvarchar(200)		
 AS
-	INSERT INTO BaiViet VALUES( @machitietquangcao, @manhanvien, @noidungbaiviet, @trangthaikiemduyet)
+	INSERT INTO BaiViet VALUES( @machitietquangcao, @manhanvien, @noidungbaiviet, @trangthaikiemduyet, @lydokhongduyet)
 GO
 
 CREATE PROC sp_BaiViet_Xoa
@@ -30,7 +31,8 @@ CREATE PROC sp_BaiViet_Sua
 	@machitietquangcao int,				
 	@manhanvien int,
 	@noidungbaiviet	nvarchar(4000),
-	@trangthaikiemduyet	int	
+	@trangthaikiemduyet	int,
+	@lydokhongduyet nvarchar(200)
 AS
-	UPDATE BaiViet SET MaChiTietQuangCao=@machitietquangcao, MaNhanVien=@manhanvien, NoiDungBaiViet=@noidungbaiviet, TrangThaiKiemDuyet=@trangthaikiemduyet WHERE MaBaiViet=@mabaiviet
+	UPDATE BaiViet SET MaChiTietQuangCao=@machitietquangcao, MaNhanVien=@manhanvien, NoiDungBaiViet=@noidungbaiviet, TrangThaiKiemDuyet=@trangthaikiemduyet, LyDoKhongDuyet = @lydokhongduyet WHERE MaBaiViet=@mabaiviet
 GO
